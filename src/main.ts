1ec76a295cb11cfc391ebbae1cdc0dc31f1d4ed1
leschuster <152649947+leschuster@users.noreply.github.com>
2024-09-07 14:13:15 +0200

    Register commands and setup processor

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import { App, Modal, Plugin, Setting } from "obsidian";
-import { AdvancedCopyPluginSettings } from "./settings/settings";
+import {
+	App,
+	Editor,
+	MarkdownFileInfo,
+	MarkdownView,
+	Modal,
+	Plugin,
+	Setting,
+} from "obsidian";
+import { AdvancedCopyPluginSettings, Profile } from "./settings/settings";
 import { DEFAULT_SETTINGS } from "./settings/default-settings";
 import { AdvancedCopyPluginSettingsTab } from "./settings/settings-ui";
+import { Logger } from "./utils/Logger";
+import { ClipboardHelper } from "./utils/ClipboardHelper";
+import { Processor } from "./processor/processor";
 
 export const PLUGIN_NAME = "Advanced-Copy";
 
@@ -31,5 +42,55 @@ export default class AdvancedCopyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	private registerCommands(): void {}
+	private registerCommands(): void {
+		if (!this.settings) {
+			Logger.error("Could not register commands: Settings not loaded");
+			return;
+		}
+
+		for (const profile of Object.values(this.settings.profiles)) {
+			console.log(profile);
+			if (profile.cmd_selection) {
+				this.createCmdToCopySelection(profile);
+			}
+
+			if (profile.code_block) {
+				this.createCmdToCopyPage(profile);
+			}
+		}
+	}
+
+	private createCmdToCopySelection(profile: Profile): void {
+		this.addCommand({
+			id: `${PLUGIN_NAME}-Profile-${profile.id}-Copy-Selection`,
+			name: `${profile.name}: Selection`,
+			editorCallback: (
+				editor: Editor,
+				ctx: MarkdownView | MarkdownFileInfo,
+			) => {
+				this.copy(editor.getSelection(), profile);
+			},
+		});
+	}
+
+	private createCmdToCopyPage(profile: Profile): void {
+		this.addCommand({
+			id: `${PLUGIN_NAME}-Profile-${profile.id}-Copy-Page`,
+			name: `${profile.name}: Page`,
+			editorCallback: (
+				editor: Editor,
+				ctx: MarkdownView | MarkdownFileInfo,
+			) => {
+				this.copy(editor.getValue(), profile);
+			},
+		});
+	}
+
+	private copy(input: string, profile: Profile): void {
+		const activeFile = this.app.workspace.getActiveFile();
+
+		const output = Processor.process(input, profile, activeFile);
+
+		ClipboardHelper.copy(output);
+	}
 }
