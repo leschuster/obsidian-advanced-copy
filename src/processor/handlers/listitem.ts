e89057a72d68064faff4a5980688b18dfd205114
leschuster <152649947+leschuster@users.noreply.github.com>
2024-09-10 21:07:10 +0200

    Add support for lists

--- src/processor/handlers/listitem.ts
+++ src/processor/handlers/listitem.ts
@@ -1,1 +1,19 @@
-export {};
+import { ListItem } from "mdast";
+import { Profile } from "src/settings/settings";
+import toCustom from "../toCustom";
+
+/**
+ * Convert a list item to string
+ * Available variables:
+ * - $value
+ * @param node
+ * @param profile
+ * @returns
+ */
+export function listItem(node: ListItem, profile: Profile): string {
+	const content = node.children
+		.map((child) => toCustom(child, { profile }))
+		.join("");
+
+	return profile.templates.listItem.replaceAll("$value", content);
+}
