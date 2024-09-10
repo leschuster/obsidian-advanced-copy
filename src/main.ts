6a97c79e0e28997fcb791f08175ec98577529a7c
leschuster <152649947+leschuster@users.noreply.github.com>
2024-09-10 19:40:44 +0200

    Work on settings UI

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,27 @@ export const PLUGIN_NAME = "Advanced-Copy";
 export default class AdvancedCopyPlugin extends Plugin {
 	public settings: AdvancedCopyPluginSettings | undefined;
 
-	async onload() {
+	/**
+	 * Plugin lifecycle hook that gets executed when the Plugin is loaded
+	 */
+	public async onload() {
 		await this.loadSettings();
 
 		this.registerCommands();
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new AdvancedCopyPluginSettingsTab(this.app, this));
 	}
 
+	/**
+	 * Plugin lifecycle hook that gets executed when the Plugin is unloaded
+	 */
 	public onunload(): void {}
 
+	/**
+	 * Load user settings
+	 */
 	public async loadSettings(): Promise<void> {
+		// TODO: DEEP COPY
 		this.settings = Object.assign(
 			{},
 			DEFAULT_SETTINGS,
@@ -30,54 +39,76 @@ export default class AdvancedCopyPlugin extends Plugin {
 		);
 	}
 
+	/**
+	 * Save user settings
+	 */
 	public async saveSettings(): Promise<void> {
+		console.log("SAVING", this.settings);
 		await this.saveData(this.settings);
 	}
 
+	/**
+	 * Register commands for each profile
+	 */
 	private registerCommands(): void {
 		if (!this.settings) {
 			Logger.error("Could not register commands: Settings not loaded");
 			return;
 		}
 
+		// Add commands for each profile individually
 		for (const profile of Object.values(this.settings.profiles)) {
 			if (profile.meta.cmdSelection) {
-				this.createCmdToCopySelection(profile);
+				this.registerCmdToCopySelection(profile);
 			}
 
 			if (profile.meta.cmdPage) {
-				this.createCmdToCopyPage(profile);
+				this.registerCmdToCopyPage(profile);
 			}
 		}
 	}
 
-	private createCmdToCopySelection(profile: Profile): void {
+	/**
+	 * Register command to copy the current selection for given profile
+	 * @param profile
+	 */
+	private registerCmdToCopySelection(profile: Profile): void {
 		this.addCommand({
 			id: `${PLUGIN_NAME}-Profile-${profile.meta.id}-Copy-Selection`,
 			name: `${profile.meta.name}: Selection`,
 			editorCallback: (
 				editor: Editor,
-				ctx: MarkdownView | MarkdownFileInfo,
+				_: MarkdownView | MarkdownFileInfo,
 			) => {
 				this.copy(editor.getSelection(), profile);
 			},
 		});
 	}
 
-	private createCmdToCopyPage(profile: Profile): void {
+	/**
+	 * Register command to copy the entire page for given profile
+	 * @param profile
+	 */
+	private registerCmdToCopyPage(profile: Profile): void {
 		this.addCommand({
 			id: `${PLUGIN_NAME}-Profile-${profile.meta.id}-Copy-Page`,
 			name: `${profile.meta.name}: Page`,
 			editorCallback: (
 				editor: Editor,
-				ctx: MarkdownView | MarkdownFileInfo,
+				_: MarkdownView | MarkdownFileInfo,
 			) => {
 				this.copy(editor.getValue(), profile);
 			},
 		});
 	}
 
+	/**
+	 * Perform advanced copy of input, using given profile
+	 * @param input text to copy
+	 * @param profile profile to use
+	 */
 	private async copy(input: string, profile: Profile): Promise<void> {
+		// We need a reference to the current file to get some global variables
 		const activeFile = this.app.workspace.getActiveFile();
 
 		const output = await Processor.process(input, profile, activeFile);
