f50600f8a4c17578b9b7352225b34c52037d49d6
leschuster <152649947+leschuster@users.noreply.github.com>
2024-09-13 10:23:22 +0200

    Add support for global variables

--- src/processor/processor.ts
+++ src/processor/processor.ts
@@ -7,7 +7,6 @@ import customStringify from "./customStringify";
 
 export class Processor {
 	private constructor(
-		private input: string,
 		private profile: Profile,
 		private activeFile: TFile | null,
 	) {}
@@ -17,19 +16,57 @@ export class Processor {
 		profile: Profile,
 		activeFile: TFile | null,
 	): Promise<string> {
-		const instance = new Processor(input, profile, activeFile);
-		return await instance.process();
+		const instance = new Processor(profile, activeFile);
+
+		const preprocessed = instance.preprocess(input);
+		const processed = await instance.process(preprocessed);
+		const postprocessed = instance.postprocess(processed);
+
+		console.log("OUTPUT:", postprocessed);
+
+		return postprocessed;
 	}
 
-	private async process(): Promise<string> {
-		const rendered = await unified()
+	private preprocess(input: string): string {
+		// Standardize line endings
+		return input.replace(/\r\n|\r/g, "\n");
+	}
+
+	private async process(input: string): Promise<string> {
+		const content = await unified()
 			.use(remarkParse)
 			.use(remarkMath)
 			.use(customStringify, { profile: this.profile })
-			.process(this.input);
+			.process(input);
 
-		console.log("OUTPUT:", String(rendered));
+		const rendered =
+			this.profile.extra.before +
+			String(content) +
+			this.profile.extra.after;
 
-		return String(rendered);
+		return rendered;
+	}
+
+	private postprocess(input: string): string {
+		return this.replaceGlobalVariables(input);
+	}
+
+	private replaceGlobalVariables(text: string): string {
+		const date = new Date();
+
+		const globalVariables: { [key: string]: string } = {
+			fileBasename: this.activeFile?.basename ?? "",
+			fileExtension: this.activeFile?.extension ?? "",
+			fileName: this.activeFile?.name ?? "",
+			filePath: this.activeFile?.path ?? "",
+			date: date.toLocaleDateString(),
+			time: date.toLocaleTimeString(),
+		};
+
+		for (const key in globalVariables) {
+			text = text.replaceAll(`\$${key}`, globalVariables[key]);
+		}
+
+		return text;
 	}
 }
